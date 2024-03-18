8dac7cd833706a40bebb66d260e93341ac2f6f64
Ionescu Catalin <cata.ionescu2003@gmail.com>
2024-03-18 17:37:56 +0200

    version1.0

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+     public static void main(String[] args){
+        Elev elev1 = new Elev();
+        System.out.println(elev1.x);
+
+   }
+}
+class Elev {
+   private int x=7;
+   private int y=9;
+
+}
